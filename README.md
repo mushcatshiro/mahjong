94c88b6595abc5acc5ffe8bbe70840732e311a47
yingjieho <yingjieho@hotmail.com>
2024-06-17 23:01:22 +0800

    add greedy player with self play 97% completetion rate

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ mahjong in python. two modes provided, visualized client and api client.
 - [ ] 24圈
   - [ ] 发牌顺序
   - [ ] 起手顺序
+  - [ ] calculate fan
 - [ ] class for each tile instead of writing out chinese character
 - [ ] performance bench
   - [ ] per round time
@@ -25,3 +26,19 @@ mahjong in python. two modes provided, visualized client and api client.
 - `test_fan.py` focus on 100% coverage of real hands
 - non `test_calculate_fan.py::test_calculate_fan` focus on `ResultFan`
 - `test_calculate_fan.py::test_calculate_fan` focus on all 82 combinations
+- [ ] elagant prints
+  - [ ] discard tiles segregated by players in sequence
+- [ ] play action
+  - [ ] move discard out? (less for loops)
+
+```bash
+>> python -m pytest --cov=. --cov-report html tests/
+```
+
+## RL thoughts
+
+predict best next board to infer action.
+how to run multiple RL agents and have gradient descent?
+4 (peng + max 3 shang combination) * 13 (discards) action space size
+using softmax to force probablity to sum up to one, and ensure
+irrelevant actions are 0
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import copy
-import random
+from typing import List
 
 from game import Player, PlayAction
 
@@ -9,61 +9,83 @@ class GreedyPlayer(Player):
     short sighted player that considers their hand only
     """
 
-    def __init__(self, player_idx, house=False, debug=False, strategy="random"):
+    def __init__(self, player_idx, house=False, weights=None):
         super().__init__(player_idx, house)
-        self.soft_locked_tiles = []
-        self.distinct_tiles = {}
-        self.conflicts = []
+        if weights is None:
+            weights = {
+                "jiang": 20,
+                "shang": 10,
+                "peng": 10,
+                "an_gang": -10,
+                "jia_gang": 10,
+                "ming_gang": 1,
+                "hu": 1000,
+                "an_ke": 100,
+                "an_shang": 80,
+                "discard": 0,
+            }
+        self.weights = weights
 
-    def preprocess_hand(self):
-        self.soft_locked_tiles = []  # reset
+    def calculate_play_action_score(self, action: PlayAction) -> int:
+        score = 0
+        if action.action == "hu":
+            return self.weights[action.action]
+        resulting_hand: list = copy.deepcopy(self.hand.tiles)
+        if self.hand.gang_history:
+            score += (
+                len(self.hand.gang_history)
+                * (self.weights["ming_gang"] + self.weights["jia_gang"])
+                / 2
+            )
+        if self.hand.peng_history:
+            score += len(self.hand.peng_history) * self.weights["peng"]
+        if self.hand.shang_history:
+            score += len(self.hand.shang_history) * self.weights["shang"]
+        if self.hand.an_gang_history:
+            score += len(self.hand.an_gang_history) * self.weights["an_gang"]
+        # action score
+        score += self.weights[action.action]
+        # remove gang/peng/shang tiles
+        if action.action == "jia_gang":
+            resulting_hand.remove(action.move_tiles[0])
+        else:
+            for tile in action.move_tiles:
+                resulting_hand.remove(tile)
+        if action.discard_tile:
+            resulting_hand.remove(action.discard_tile)
+        # remaining score
+        score += len(self.hand.get_valid_jiangs(resulting_hand)) * self.weights["jiang"]
+        score += (
+            len(self.hand.get_valid_peng_sets(resulting_hand)) * self.weights["an_ke"]
+        )
+        score += (
+            len(self.hand.get_valid_shang_sets(resulting_hand))
+            * self.weights["an_shang"]
+        )
+        return score
 
-        tiles = copy.deepcopy(self.hand.tiles)
-
-        self.soft_locked_tiles += self.hand.get_valid_jiangs(tiles)
-
-        shang_sets = self.hand.get_valid_shang_sets(tiles)
-        for shang_set in shang_sets:
-            self.soft_locked_tiles += shang_set
-
-        peng_sets = self.hand.get_valid_peng_sets(tiles)
-        for peng_set in peng_sets:
-            self.soft_locked_tiles += peng_set
-        self.soft_locked_tiles = list(set(self.soft_locked_tiles))
-
-    def trim_possible_actions(self, possible_actions):
-        trimmed_possible_actions = []
-        for action in possible_actions:
-            action: PlayAction
-            if action.discard_tile and action.discard_tile in self.soft_locked_tiles:
-                continue
-            trimmed_possible_actions.append(action)
-        return trimmed_possible_actions
-
-    def play_turn_strategy(self, possible_actions) -> PlayAction:
-        """
-        TODO dealing with tiles used in multiple sets
-        peng, shang, eyes
-        shang + peng is ok to keep all
-        shang + peng + gang need to think
-        """
-        self.preprocess_hand()
-        trimmed_possible_actions = self.trim_possible_actions(possible_actions)
-        return random.choice(trimmed_possible_actions)
+    def play_turn_strategy(self, possible_actions: List[PlayAction]) -> PlayAction:
+        play_action_scores = {}
+        for idx, action in enumerate(possible_actions):
+            play_action_scores[idx] = self.calculate_play_action_score(action)
+        max_play_action_score = max(play_action_scores, key=play_action_scores.get)
+        return possible_actions[max_play_action_score]
 
     def call_strategy(self, possible_actions, played_tile, **kwargs) -> PlayAction:
-        if not possible_actions:
-            return []
-        for action in possible_actions:
-            if action.action == "hu":
-                return action
-        # BUG remove locked discards then randomly choose?
-        return random.choice(possible_actions)
+        call_action_scores = {}
+        for idx, action in enumerate(possible_actions):
+            call_action_scores[idx] = self.calculate_play_action_score(action)
+        max_call_action_score = max(call_action_scores, key=call_action_scores.get)
+        return possible_actions[max_call_action_score]
 
     def gang_discard_strategy(self, possible_actions) -> PlayAction:
-        self.preprocess_hand()
-        trimmed_possible_actions = self.trim_possible_actions(possible_actions)
-        return random.choice(trimmed_possible_actions)
+        gang_discard_action_scores = {}
+        for idx, action in enumerate(possible_actions):
+            gang_discard_action_scores[idx] = self.calculate_play_action_score(action)
+        max_gang_discard_action_score = max(
+            gang_discard_action_scores, key=gang_discard_action_scores.get
+        )
+        return possible_actions[max_gang_discard_action_score]
 
 
 class AssistedPlayer:
